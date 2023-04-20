9e119d0f657e5b7461ac1e8ccb785f60150aaff3
saiki <saiki@LAPTOP-6UU6T67A>
2023-04-20 19:49:46 +0530

    2 commit

---
+++ PracticeMaven/src/test/java/Maven/SmokeExTest.java
@@ -0,0 +1,12 @@
+package Maven;
+
+import org.testng.annotations.Test;
+
+public class SmokeExTest {
+
+	@Test(groups = "smoketest")
+	public void m1()
+	{
+		System.out.println("method1 running");
+	}
+}
